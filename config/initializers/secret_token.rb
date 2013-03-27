479384c57e6fc79201fffb08760e7dfd215524a4
Jared Hanstra <jhanstra@gmail.com>
2013-03-26 23:56:06 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ndconfessions::Application.config.secret_token = 'eb6491e6ba057ab6e44f887e68577cf540214585d0b9e088c53616b92553eb6d35fe68c29550fba6489c8337955cff2afde197c9b7d497fbbfe635e7df6c0ee0'
