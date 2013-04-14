93f4741a04fa928d17a9cc684afc8e28791587ff
Alec Hipshear <alechipshear@gmail.com>
2013-04-14 17:36:38 -0400

    Add some fun queries to get popular confessions

--- app/models/confession.rb
+++ app/models/confession.rb
@@ -3,4 +3,18 @@ class Confession < ActiveRecord::Base
   validates :content, presence: true
 
   acts_as_voteable
+
+  scope :popular, lambda {|start_date=Date.today, end_date=Date.today.end_of_day|
+    plusminus_tally
+      .where('votes.created_at > :start_date AND votes.created_at <= :end_date',
+             {start_date: start_date, end_date: end_date})
+  }
+
+  scope :popular_this_week, lambda {
+    popular(7.days.ago)
+  }
+
+  scope :popular_this_month, lambda {
+    popular(30.days.ago)
+  }
 end
